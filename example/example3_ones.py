9dc9fc21877fb67094b472b9cdadcb896a209b50
k9 <m0ssy.00.m0ss@gmail.com>
2026-05-12 21:16:56 +0300

    update + foldered

--- example/example3_ones.py
+++ example/example3_ones.py
@@ -3,8 +3,8 @@ import numpy as np
 # np.ones() creates an array where
 # every value equals 1.
 
-ones-array = np.ones((2, 2))
+ones_array = np.ones((2, 2))
 
 # print the array
 print("array filled with ones:")
-print(ones-array)
+print(ones_array)
