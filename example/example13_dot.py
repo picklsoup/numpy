7e8f42ee15b64c6b3794783100052b20befc2873
k9 <m0ssy.00.m0ss@gmail.com>
2026-05-12 21:13:17 +0300

    update + foldered

---
+++ example/example13_dot.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# create two vectors
+vector_a = np.array([1, 2])
+vector_b = np.array([3, 4])
+
+# np.dot() multiplies vectors
+
+dot_product = np.dot(vector_a, vector_b)
+
+# print vectors
+print("vector a:", vector_a)
+print("vector b:", vector_b)
+
+# print result
+print("dot product:", dot_product)
